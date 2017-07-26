c6e6db4dae6b2f39dab3103f0586b01f65f39474
yuralala8 <yc1435@nyu.edu>
2017-07-25 20:36:21 -0400

    join the fun rails

---
+++ db/migrate/20170726003521_add_columns_to_rides.rb
@@ -0,0 +1,8 @@
+class AddColumnsToRides < ActiveRecord::Migration
+  def change
+    add_reference :rides, :taxi, index: true
+    add_foreign_key :rides, :taxis
+    add_reference :rides, :passenger, index: true
+    add_foreign_key :rides, :passengers
+  end
+end
